2fcb1f5a5c80648353e9d9a9b1ef7f01fd4f095b
jade-emperror <maestrobharath@gmail.com>
2020-12-12 17:36:06 +0530

    first 2 java program

---
+++ sumoftwonum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class sumoftwonum {
+    public static void main(String args[]) {
+    Scanner ip = new Scanner(System.in);    
+    System.out.println("Enter A value");
+    Integer a = ip.nextInt();
+    System.out.println("Enter B value");
+    Integer b = ip.nextInt();
+    System.out.println("sum of two numbere "+(a+b));
+}
+}
